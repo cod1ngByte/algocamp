316d15781420b965e8608424231b902419625084
cod1ngByte <rajiv.ranjansoc@gmail.com>
2024-08-12 23:14:24 +0530

    restruct code

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,24 +1,38 @@
-let m = 0;
-let mm = 0;
-let hh = 0;
-let h = 0;
-const id = setInterval(function () {
-    // console.log(count);
+// let m = 0;
+// let mm = 0;
+// let hh = 0;
+// let h = 0;
+// const id = setInterval(function () {
+//     // console.log(count);
 
-    console.log(hh, mm);
-    m++;
-    if (m == 7) {
-        h++;
-        hh = h;
-        m = 0;
-    }
-    mm = m;
-    if (hh == 3) {
-        m = 0;
-        h = 0;
-        mm = m;
-        hh = h;
-        console.log(hh, mm);
-        clearInterval(id);
-    }
-}, 1000);
+//     console.log(hh, mm);
+//     m++;
+//     if (m == 7) {
+//         h++;
+//         hh = h;
+//         m = 0;
+//     }
+//     mm = m;
+//     if (hh == 3) {
+//         m = 0;
+//         h = 0;
+//         mm = m;
+//         hh = h;
+//         console.log(hh, mm);
+//         clearInterval(id);
+//     }
+// }, 1000);
+
+const f = function () {
+    console.log("file getting downloaded");
+    setTimeout(() => {
+        console.log("file downloaded");
+        // console.log('')
+        setTimeout(() => {
+            console.log("file updated");
+            setTimeout(() => {
+                console.log("file uploaded");
+            }, 3000);
+        }, 2000);
+    }, 1000);
+};
