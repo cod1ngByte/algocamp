5dfece1804610495fb197aa018ba28b6716cea17
cod1ngByte <rajiv.ranjansoc@gmail.com>
2024-08-25 15:14:07 +0530

    add task to html

--- DOM/01_Todo/script.js
+++ DOM/01_Todo/script.js
@@ -13,18 +13,25 @@ function loadTodos() {
     // const todo = localStorage.getItem("todos");
     // console.log(todo);
     const todos = JSON.parse(localStorage.getItem("todos")) || { todoList: [] };
-    console.log(todos);
+    // console.log(todos);
     return todos;
 }
-function addTodoToLocalStorage(text) {
+function addTodoToLocalStorage(todoText) {
     //
     const todos = loadTodos();
-    todos.todoList.push(text);
+    todos.todoList.push(todoText);
     localStorage.setItem("todos", JSON.stringify(todos));
-    console.log("added");
+    // console.log("added");
 }
-
+function appendTodoInHtml(todoText) {
+    //
+    const todoList = document.getElementById("taskList");
+    const li = document.createElement("li");
+    li.textContent = todoText;
+    todoList.appendChild(li);
+}
 document.addEventListener("DOMContentLoaded", () => {
+    //
     // console.dir(document.getElementById("todoInput"));
     /*
     //get input value
@@ -45,17 +52,30 @@ document.addEventListener("DOMContentLoaded", () => {
     // });
     const todoInput = document.getElementById("todoInput");
     const submitButton = document.getElementById("addTodo");
+    const todoList = document.getElementById("taskList");
+
+    todoInput.addEventListener("change", (e) => {
+        //
+        const todoText = e.target.value;
+        e.target.value = todoText.trim();
+    });
     submitButton.addEventListener("click", (e) => {
+        //
         const todoText = todoInput.value;
         if (todoText === "") {
             alert("add task");
         } else {
             addTodoToLocalStorage(todoText);
+            appendTodoInHtml(todoText);
         }
+        todoInput.value = "";
     });
-    todoInput.addEventListener("change", (e) => {
-        const todoText = e.target.value;
-        e.target.value = todoText.trim();
+
+    const todos = loadTodos();
+    todos.todoList.forEach((todoItem) => {
+        const li = document.createElement("li");
+        li.textContent = todoItem;
+
+        todoList.appendChild(li);
     });
-    loadTodos();
 });
