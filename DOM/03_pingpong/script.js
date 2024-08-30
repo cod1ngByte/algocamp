076781c30ae81be9a2cf6fe7cf0d0070e09a33f3
cod1ngByte <rajiv.ranjansoc@gmail.com>
2024-08-30 18:41:50 +0530

    add ballMovement()

--- DOM/03_pingpong/script.js
+++ DOM/03_pingpong/script.js
@@ -9,6 +9,24 @@ document.addEventListener("DOMContentLoaded", function () {
      let intervalId;
      const ball = { x: 200, y: 200 };
      const table = { x: 0, y: 180 };
+     const gameArenaWidth = 600;
+     const gameArenaHeight = 600;
+     const ballWidth = 20;
+     const ballHeight = 20;
+     let bx = 1;
+     let by = -1;
+
+     function ballMovement() {
+          // const ballEle = document.querySelector(".ball");
+          if (ball.x + bx > gameArenaWidth - ballWidth || ball.x + bx < 0) {
+               bx = -bx; //change the direction of x
+          }
+          if (ball.y + by > gameArenaHeight - ballHeight || ball.y + by < 0) {
+               by = -by; // change the direction of y
+          }
+          ball.x = ball.x + bx;
+          ball.y = ball.y + by;
+     }
 
      function drawDiv(xCoord, yCoord, className) {
           const div = document.createElement("div");
@@ -27,24 +45,25 @@ document.addEventListener("DOMContentLoaded", function () {
           gameArena.append(ballEle);
      }
 
+     function gameLoop() {
+          intervalId = setInterval(() => {
+               ballMovement();
+               drawBallAndTable();
+          }, 10);
+     }
+
      function startGame() {
           startBtn.style.display = "none";
           isGameStarted = true;
-          // gameLoop(); // Todo
-          drawBallAndTable();
-          moveBall();
+          gameLoop();
      }
-     // function gameLoop(){
-     //      intervalId = setInterval(()=>{
-     //           moveBall();
-     //           drawBallAndTable();
-     //      },200)
-     // }
 
      function init() {
           startBtn.textContent = "Start";
           startBtn.classList.add("start-button");
           document.body.insertBefore(startBtn, script);
+
+          //--> start button event
           startBtn.addEventListener("click", startGame);
      }
      init();
