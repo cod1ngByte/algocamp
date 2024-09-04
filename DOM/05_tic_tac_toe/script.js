c0244d09015ac4665d3d5508b45d1f1fd350a89d
cod1ngByte <rajiv.ranjansoc@gmail.com>
2024-09-04 09:28:58 +0530

    update game areana

--- DOM/05_tic_tac_toe/script.js
+++ DOM/05_tic_tac_toe/script.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
      console.log("loaded");
+     const player = document.querySelector(".player");
+     const winPlayer = document.querySelector(".win");
+     const gameArena = document.querySelector(".game-arena");
 });
