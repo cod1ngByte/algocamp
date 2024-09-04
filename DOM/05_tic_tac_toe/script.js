7e8271bc65bf43b46fc04b8996a18edd51b14f45
cod1ngByte <rajiv.ranjansoc@gmail.com>
2024-09-04 12:05:12 +0530

    add draw box and mark cross or circle feature

--- DOM/05_tic_tac_toe/script.js
+++ DOM/05_tic_tac_toe/script.js
@@ -3,4 +3,23 @@ document.addEventListener("DOMContentLoaded", function () {
      const player = document.querySelector(".player");
      const winPlayer = document.querySelector(".win");
      const gameArena = document.querySelector(".game-arena");
+     const boxes = ["", "", "", "", "", "", "", "", ""];
+
+     let playerTurn = "X";
+
+     function drawBox() {
+          boxes.forEach((box, index) => {
+               const boxEle = document.createElement("div");
+               boxEle.classList.add("box");
+               boxEle.id = index;
+               boxEle.addEventListener("click", markBoxWithCrossOrCircle);
+               gameArena.append(boxEle);
+          });
+     }
+     drawBox();
+     function markBoxWithCrossOrCircle(e) {
+          console.log(e.target);
+          e.target.textContent = playerTurn;
+          playerTurn = playerTurn === "X" ? "O" : "X";
+     }
 });
